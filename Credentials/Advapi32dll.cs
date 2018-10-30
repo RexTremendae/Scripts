9b9e14ef2b1384f59107ba8bae42bb5c48350097
Per Westerlund <per@fastmail.com>
2018-10-30 20:26:41 +0100

    [Misc]       Added p/invoke code for windows credentials management

---
+++ Credentials/Advapi32dll.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Credentials
+{
+    public static class Advapi32dll
+    {
+        private const string Advapi32Dll = "advapi32.dll";
+
+        public static (bool success, string username, string password) GetCredentials(string target, CRED_TYPE credType = CRED_TYPE.GENERIC)
+        {
+            var username = string.Empty;
+            var password = string.Empty;
+            var credentialPtr = IntPtr.Zero;
+
+            try
+            {
+                bool success = Advapi32dll.CredRead(target, Advapi32dll.CRED_TYPE.GENERIC, 0, out credentialPtr);
+                if (success)
+                {
+                    var credential = (Advapi32dll.CREDENTIAL)Marshal.PtrToStructure(credentialPtr, typeof(Advapi32dll.CREDENTIAL));
+                    username = credential.UserName;
+
+                    if (credential.CredentialBlobSize > 2)
+                    {
+                        password = Marshal.PtrToStringUni(credential.CredentialBlob, (int)credential.CredentialBlobSize / 2);
+                    }
+                }
+
+                return (success, username, password);
+            }
+            finally
+            {
+                if (credentialPtr != IntPtr.Zero)
+                {
+                    CredFree(credentialPtr);
+                }
+            }
+        }
+
+        public static (bool success, int errorCode) StoreCredentials(string target, string username, string password, CRED_TYPE type = CRED_TYPE.GENERIC)
+        {
+            byte[] byteArray = Encoding.Unicode.GetBytes(password);
+
+            if (byteArray.Length > 512)
+            {
+                throw new ArgumentOutOfRangeException("The secret message has exceeded 512 bytes.");
+            }
+
+            CREDENTIAL credentailPtr = new CREDENTIAL
+            {
+                AttributeCount = 0,
+                CredAttribute = IntPtr.Zero,
+                Comment = null,
+                TargetAlias = null,
+                Type = type,
+                Persist = (UInt32)CRED_PERSIST.ENTERPRISE,
+                CredentialBlobSize = (UInt32)Encoding.Unicode.GetBytes(password).Length,
+                TargetName = target,
+                CredentialBlob = Marshal.StringToCoTaskMemUni(password),
+                UserName = username
+            };
+
+            bool written = CredWrite(ref credentailPtr, 0);
+            int lastError = Marshal.GetLastWin32Error();
+
+            if (written)
+            {
+                return (true, 0);
+            }
+
+            return (false, lastError);
+        }
+
+        public static IEnumerable<string> ListCredentialTargets()
+        {
+            var credentialsListPtr = IntPtr.Zero;
+
+            try
+            {
+                var status = CredEnumerate(null, 0x1, out int count, out credentialsListPtr);
+
+                if (status)
+                {
+                    for (int n = 0; n < count; n++)
+                    {
+                        IntPtr credentialPtr = Marshal.ReadIntPtr(credentialsListPtr, n * Marshal.SizeOf(typeof(IntPtr)));
+                        var credential = (CREDENTIAL)Marshal.PtrToStructure(credentialPtr, typeof(CREDENTIAL));
+
+                        yield return credential.TargetName;
+                    }
+                }
+                else
+                {
+                    int lastError = Marshal.GetLastWin32Error();
+                    throw new Exception($"Error code: {lastError}");
+                }
+            }
+            finally
+            {
+                if (credentialsListPtr != IntPtr.Zero)
+                {
+                    CredFree(credentialsListPtr);
+                }
+            }
+        }
+
+        public static bool DeleteCredentials(string target, CRED_TYPE type = CRED_TYPE.GENERIC)
+        {
+            return CredDelete(target, type, 0);
+        }
+
+        [StructLayout(LayoutKind.Seque­ntial, CharSet=CharSet.Unicode)]
+        public struct CREDENTIAL
+        {
+            public UInt32 Flags;
+            public CRED_TYPE Type;
+            public string TargetName;
+            public string Comment;
+            public FILETIME LastWritten;
+            public UInt32 CredentialBlobSize;
+            public IntPtr CredentialBlob;
+            public UInt32 Persist;
+            public UInt32 AttributeCount;
+            public IntPtr CredAttribute;
+            public string TargetAlias;
+            public string UserName;
+
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FILETIME {
+            public uint DateTimeLow;
+            public uint DateTimeHigh;
+        }
+
+        public enum CRED_TYPE : uint
+        {
+            GENERIC = 1,
+            DOMAIN_PASSWORD = 2,
+            DOMAIN_CERTIFICATE = 3,
+            DOMAIN_VISIBLE_PASSWORD = 4,
+            GENERIC_CERTIFICATE = 5,
+            DOMAIN_EXTENDED = 6,
+            MAXIMUM = 7,                    // Maximum supported cred type
+            MAXIMUM_EX = (MAXIMUM + 1000),  // Allow new applications to run on old OSes
+        }
+
+        public enum CRED_PERSIST : uint
+        {
+            SESSION = 1,
+            LOCAL_MACHINE = 2,
+            ENTERPRISE = 3,
+        }
+
+        [DllImport(Advapi32Dll, EntryPoint="CredReadW", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern bool CredRead(string target, CRED_TYPE type, int reservedFlag, out IntPtr credentialPtr);
+
+        [DllImport(Advapi32Dll, EntryPoint = "CredWriteW", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern bool CredWrite([In] ref CREDENTIAL userCredential, [In] UInt32 flags);
+
+        [DllImport(Advapi32Dll, EntryPoint = "CredFree", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern void CredFree(IntPtr credentialPtr);
+
+        [DllImport(Advapi32Dll, EntryPoint = "CredEnumerateW", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern bool CredEnumerate(string filter, int flag, out int count, out IntPtr credentials);
+
+        [DllImport(Advapi32Dll, EntryPoint = "CredDeleteW", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern bool CredDelete(string target, CRED_TYPE type, int flag);
+    }
+}
---
+++ Credentials/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+using static System.Console;
+
+namespace Credentials
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Run(args);
+        }
+
+        public void Run(string[] args)
+        {
+            WriteLine();
+
+            var cmd = GetCommand(args);
+
+            if (cmd == Command.Unknown)
+            {
+                PrintCommandUsage();
+                return;
+            }
+
+            args = args.Skip(1).ToArray();
+
+            switch (cmd)
+            {
+                case Command.Get:
+                    GetCredentials(args);
+                    break;
+
+                case Command.Add:
+                    AddCredentials(args);
+                    break;
+
+                case Command.List:
+                    ListCredentials(args);
+                    break;
+
+                case Command.Delete:
+                    DeleteCredentials(args);
+                    break;
+            }
+
+            WriteLine();
+        }
+
+        private void ListCredentials(string[] args)
+        {
+            foreach (var target in Advapi32dll.ListCredentialTargets())
+            {
+                var splitIdx = target.IndexOf('=');
+
+                if (splitIdx < 0)
+                {
+                    Console.WriteLine(target);
+                }
+                else
+                {
+                    Write(target.Substring(0, splitIdx+1));
+                    ColorWriteLine(target.Substring(splitIdx+1, target.Length-splitIdx-1), ConsoleColor.Yellow);
+                }
+            }
+        }
+
+        private void AddCredentials(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Write("Please provide ");
+                ColorWrite("target", ConsoleColor.Yellow);
+                Write(", ");
+                ColorWrite("username", ConsoleColor.Yellow);
+                Write(" and ");
+                ColorWrite("password", ConsoleColor.Yellow);
+                WriteLine(".");
+
+                return;
+            }
+
+            var (success, errorCode) = Advapi32dll.StoreCredentials(args[0], args[1], args[2]);
+
+            if (success)
+            {
+                ColorWriteLine("Credentials written successfully!", ConsoleColor.Green);
+            }
+            else
+            {
+                ColorWrite("ERROR: ", ConsoleColor.Red);
+                WriteLine("Failed to write credentials!");
+                Write("Error code: ");
+                ColorWrite(errorCode.ToString(), ConsoleColor.Red);
+            }
+        }
+
+        private void DeleteCredentials(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Write("Please provide ");
+                ColorWrite("target", ConsoleColor.Yellow);
+                WriteLine(".");
+                return;
+            }
+
+            var success = Advapi32dll.DeleteCredentials(args[0]);
+
+            if (!success)
+            {
+                ColorWriteLine("Could not delete credential.", ConsoleColor.Red);
+                return;
+            }
+
+            ColorWrite("Success! ", ConsoleColor.Green);
+            WriteLine("Credential deleted.");
+        }
+
+        private void GetCredentials(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Write("Please provide ");
+                ColorWrite("target", ConsoleColor.Yellow);
+                WriteLine(".");
+                return;
+            }
+
+            var (success, username, password) = Advapi32dll.GetCredentials(args[0]);
+
+            if (!success)
+            {
+                ColorWriteLine("Could not find credential.", ConsoleColor.Red);
+                return;
+            }
+
+            Write("Username: ");
+            ColorWriteLine(username, ConsoleColor.Green);
+
+            Write("Password: ");
+            ColorWriteLine(password, ConsoleColor.Green);
+        }
+
+        private static void ColorWrite(string text, ConsoleColor color)
+        {
+            var oldColor = ForegroundColor;
+
+            try
+            {
+                ForegroundColor = color;
+                Write(text);
+            }
+            finally
+            {
+                ForegroundColor = oldColor;               
+            }
+        }
+
+        private static void ColorWriteLine(string text, ConsoleColor color)
+        {
+            var oldColor = ForegroundColor;
+
+            try
+            {
+                ForegroundColor = color;
+                WriteLine(text);
+            }
+            finally
+            {
+                ForegroundColor = oldColor;
+            }
+        }
+
+        private Command GetCommand(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                return Command.Unknown;
+            }
+
+            if (Enum.TryParse(args[0], true, out Command cmd))
+            {
+                return cmd;
+            }
+
+            return Command.Unknown;
+        }
+
+        private static void PrintCommandUsage()
+        {
+            Write("Please provide command: ");
+
+            var availableCommandsArray = Enum.GetValues(typeof(Command));
+            var availableCommands = new List<Command>();
+
+            foreach (Command arrayCmd in availableCommandsArray)
+            {
+                if (arrayCmd != Command.Unknown)
+                {
+                    availableCommands.Add(arrayCmd);
+                }
+            }
+
+            bool first = true;
+            foreach (var cmd in availableCommands.Take(availableCommands.Count - 1))
+            {
+                if (!first) Write(", ");
+                ColorWrite(cmd.ToString(), ConsoleColor.Yellow);
+                first = false;
+            }
+
+            Write(" or ");
+            ColorWrite(availableCommands.Last().ToString(), ConsoleColor.Yellow);
+            WriteLine();
+            WriteLine();
+        }
+
+        private enum Command
+        {
+            Unknown = 0,
+            List,
+            Get,
+            Add,
+            Delete
+        }
+    }
+}
