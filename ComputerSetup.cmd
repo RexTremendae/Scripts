2bc1bd16fb69168b91be9b5f70d100993a8487fc
Per Westerlund <per@fastmail.com>
2019-12-14 14:47:36 +0100

    [Comp Setup] Updated computer setup script

--- ComputerSetup.cmd
+++ ComputerSetup.cmd
@@ -4,14 +4,13 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 :: Misc. utilities
+cinst git -y
+cinst visualstudiocode -y
 cinst notepadplusplus -y
-cinst paint.net -y
 cinst 7zip -y
 cinst vlc -y
 cinst winscp -y
-cinst git -y
 cinst linqpad -y
-cinst visualstudiocode -y
-cinst conemu -y
 cinst slack -y
+cinst paint.net -y
 cinst spotify -y
