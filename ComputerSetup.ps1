70940a083246895fe34c4364f6e74a3c6cc3de5b
Per Westerlund <per@fastmail.com>
2020-02-08 21:33:41 +0100

    [Comp Setup] Replaced ComputerSetup.cmd with powershell script

---
+++ ComputerSetup.ps1
@@ -0,0 +1,45 @@
+$apps =
+"git",
+"visualstudiocode",
+"notepadplusplus",
+"7zip",
+"vlc",
+"winscp",
+"linqpad",
+"slack",
+"paint.net",
+"spotify"
+
+
+function FirstToUpper ($text)
+{
+    "$($text.Substring(0, 1).ToUpper())" + "$($text.Substring(1))"
+}
+
+function Write-Title($text)
+{
+    $line = "===="
+    for ($i = 0; $i -lt $text.Length; $i++) {
+        $line += "="
+    }
+
+    Write-Host
+    Write-Host $line -ForegroundColor White
+    Write-Host "  $text" -ForegroundColor Cyan
+    Write-Host $line -ForegroundColor White
+    Write-Host
+}
+
+Write-Title "Installing Chocolatey"
+Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+$env:Path += "%ALLUSERSPROFILE%\chocolatey\bin"
+
+$count = 1
+$total = $apps.Length
+
+$apps | ForEach-Object {
+    Write-Title "Installing $(FirstToUpper $_) ($count/$total)"
+    cinst $_ -y
+    $count = $count + 1
+}
+
