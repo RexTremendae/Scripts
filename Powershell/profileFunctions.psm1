b780ff47b704256c3d8baad559b60378e9fc04f0
Per Westerlund <per@fastmail.se>
2023-10-07 11:34:12 +0200

    [PS Config]  Clear-NugetCache

--- Powershell/profileFunctions.psm1
+++ Powershell/profileFunctions.psm1
@@ -3,4 +3,5 @@ Function .. { Set-Location .. }
 Function ... { ..; .. }
 Function .... { ..; ..; .. }
 Function ..... { ..; ..; ..; .. }
+Function Clear-NugetCache { c:\dev\nuget.exe locals all -clear }
 Function Reset-Explorer { Stop-Process (Get-Process -Name explorer).Id }
